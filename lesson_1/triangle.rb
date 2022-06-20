8b2cb7a5b0172f9ead7f177649cd9b3b234e744b
Pavel <p.kurinnoi@gmail.com>
2022-06-20 21:15:23 +0300

    pk-0-lesson_1-task_2

---
+++ lesson_1/triangle.rb
@@ -0,0 +1,8 @@
+puts "What is the base of the triangle (sm)?"
+base = gets.chomp.to_f
+
+puts "What is the height of the triangle (sm)?"
+height = gets.chomp.to_f
+
+square = (base * height) / 2
+puts "Triangle area is #{square} ssm2"
