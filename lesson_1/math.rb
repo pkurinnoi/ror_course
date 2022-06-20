67ee3da13e21f877c4b859ae7c2f994a24427eca
Pavel <p.kurinnoi@gmail.com>
2022-06-20 23:14:52 +0300

    pk-0-lesson_1-task_4

---
+++ lesson_1/math.rb
@@ -0,0 +1,37 @@
+a = []
+
+puts "Input A"
+a[0] = gets.chomp.to_i
+
+puts "Input B"
+a[1] = gets.chomp.to_i
+
+puts "Input C"
+a[2] = gets.chomp.to_i
+
+@discr = (a[1] * a[1]) - (4 * a[0] * a[2])
+@root1 = (-a[1] + Math.sqrt(@discr)) / (2 * a[0])
+@root2 = (-a[1] - Math.sqrt(@discr)) / (2 * a[0])
+
+def print_discr
+  puts "Discrymenant = #{@discr}"
+end
+
+def print_root1
+  puts "Root 1 (x1) = #{@root1}"
+end
+
+def print_root2
+  puts "Root 2 (x2) = #{@root2}"
+end
+
+if @discr > 0
+    print_discr
+    print_root1
+    print_root2
+  elsif @discr < 0
+    print_discr
+  else
+    print_discr
+    print_root1
+end
