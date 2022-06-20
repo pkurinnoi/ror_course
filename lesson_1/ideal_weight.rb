959861cf31d9c5c2f356542120395ba8ae9cd1a4
Pavel <p.kurinnoi@gmail.com>
2022-06-20 21:03:26 +0300

    pk-0-lesson_1-task_1

---
+++ lesson_1/ideal_weight.rb
@@ -0,0 +1,12 @@
+puts "Hello! What is your name?"
+name = gets.chomp
+
+puts "What is your height?"
+height = gets.chomp.to_f
+
+if (height < 110)
+    puts "#{name}, your weight is ideal"
+   else
+    puts "#{name}, your ideal weight is #{(height - 110) * 1.15} kg"
+end
+
