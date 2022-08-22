1f2d9786d8b87082f0c65d93a8f4a809be15afc0
Pavel <p.kurinnoi@gmail.com>
2022-08-22 22:42:32 +0300

    pk0-lesson_10-metaprogramming

--- lesson_10/validation_m.rb
+++ lesson_10/validation_m.rb
@@ -1,22 +1,73 @@
 module Validation
-  def validate(name, type, params = nil)
-    case type
-    when 'presense' then presense(name)
-    when 'format' then format(name, params)
-    when 'type' then type(name, params)
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    def validations
+      @validations ||= []
+    end
+    def validate(attribute, validation_type, validation_mask = nil)
+      validations.push({attr: attribute, type: validation_type, mask: validation_mask})
     end
   end
 
-  def presense(name)
-    name.nil? && name.empty?
+  module InstanceMethods
+    def valid?
+      validate!
+      true
+    rescue StandardError
+      false
+    end
+
+    def validate!(options = nil)
+      source = self.class.superclass == Object ? self.class : self.class.superclass
+      checks = source.instance_variable_get("@validations")
+
+      checks.each do |hash|
+        val = self.instance_variable_get("@#{hash[:attr]}")
+        option = hash[:mask] ? hash[:mask] : options
+        send("validate_#{hash[:type].to_s}".to_sym, hash[:attr],  val, option)
+      end
+    end
   end
 
-  def format
-
+  def presense(name, value, _)
+    raise "#{name} value is nil" if value.nil?
+    raise "#{name} value is empty" if value.to_s == ""
   end
 
-  def type
-
+  def format(name, value, params)
+    raise "#{name} with value #{value} do not fir: #{params.to_s}" if value.to_s !~ params
   end
 
+  def type(name, value, params)
+    raise "#{name} with value #{value} has different class #{value.class} than #{params}" if value.class.to_s != type.to_s
+  end
+
+  def validate_length(name, value, params)
+    raise "Length #{value} is bad" unless value.to_s.length.between?(params.first, params.last)
+  end
+
+  def validate_not_yet_existed(name, value, params)
+    raise "Object #{name} with #{value} already exist!" if params.any? do |i|
+      i.instance_variable_get("@#{name}") == value
+    end
+  end
+
+  def validate_existed(name, value, params)
+    raise "=Object #{name} with #{value} does not exist!" if params.none? do |i|
+      i.instance_variable_get("@#{name}") == value
+    end
+end
+
+
+class Y
+  require_relative 'accessors_m'
+
+  extend Validation
+  extend Accessors
+
+  attr_accessor_with_history :a, :b, :c, :d, :e, :f
 end
